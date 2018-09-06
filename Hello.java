1ee38f34df82ea26e1fe1a8cd4229e3752d56e64
16ec041 <42169930+16ec041@users.noreply.github.com>
2018-09-06 11:37:49 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Hello
+ {
+    public static void main(String[] args)
+ {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        print("Enter a n: ");
+        print("You have entered " + scanner.nextInt());
+        for(int i=0;i<=n;i++)
+         {
+            System.out.println("hello");
+         }
+}
+} 
