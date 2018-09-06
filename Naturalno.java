267741aa253f145978192ea3367de4a3cc98177b
16ec041 <42169930+16ec041@users.noreply.github.com>
2018-09-06 11:46:22 +0530

    Create  Naturalno.java

---
+++ Naturalno.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Naturalno
+ {
+    public static void main(String[] args)
+ {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.println("enter the value of n:")
+        int  sum = 0;
+        for(int i = 1; i <= num; ++i)
+        {
+            sum = sum + i;   
+        }
+
+        System.out.println("Sum = " + sum);
